33d43e5f64b073fb5f63faa658d0b39bd5a89dc3
jgartee <garteejohn@johndeere.com>
2015-11-01 08:47:37 -0500

    Create empty project

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+ 
+gem 'rspec'
+gem 'guard'
+gem 'guard-rspec'
+gem 'pry'
+gem 'gimme'
+gem 'simplecov'
+gem 'simplecov-cobertura'
+gem 'nokogiri'
---
+++ lib/game.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+
+module BowlingGame
+  class Game
+  	# def initialize
+	  # 	@score=0
+    #   @pins=Array.new
+    # end
+
+   #  def pins rollArray
+   #    rollArray.each { |roll| @pins << roll }
+   #  end
+
+   #  def score
+   #    0
+   #  end
+	end
+end
