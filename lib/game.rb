dcfd5e3cf465251ad31126b8f33cbe28fea94473
jgartee <garteejohn@johndeere.com>
2015-11-01 08:51:55 -0500

    remove comments

--- lib/game.rb
+++ lib/game.rb
@@ -2,17 +2,5 @@ require 'nokogiri'
 
 module BowlingGame
   class Game
-  	# def initialize
-	  # 	@score=0
-    #   @pins=Array.new
-    # end
-
-   #  def pins rollArray
-   #    rollArray.each { |roll| @pins << roll }
-   #  end
-
-   #  def score
-   #    0
-   #  end
 	end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,10 +7,11 @@ describe BowlingGame::Game do
       @pins = Array.new      
     end
 
-    # it "should return 0 for rolling all gutter balls" do 
-    #   (0..19).each { |roll| @pins << roll } 
-    #   @game.pins(@pins)
-    #   expect(@game.score).to eq(0)
-    # end
+    it "should return 0 for rolling all gutter balls" do 
+      expect(true).to eq(false)
+      # (0..19).each { |roll| @pins << roll } 
+      # @game.pins(@pins)
+      # expect(@game.score).to eq(0)
+    end
   end
 end
